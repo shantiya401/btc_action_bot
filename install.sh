c36e9d51b14e8435409e5f801b0da3065dd2622a
shantiya401 <123131447+shantiya401@users.noreply.github.com>
2024-07-30 03:58:52 +0330

    Update install.sh
    Token

--- install.sh
+++ install.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Update package list and install required packages
-echo "Updating package list..."
-sudo apt-get update
+# Request for Telegram API Token
+read -p "Please enter your Telegram API Token: " TELEGRAM_API_TOKEN
 
-echo "Installing required packages..."
-sudo apt-get install -y python3 python3-pip git
-
-# Remove the existing directory if it exists
-if [ -d "btc_action_bot" ]; then
-  echo "Removing existing btc_action_bot directory..."
-  rm -rf btc_action_bot
+# Check if TELEGRAM_API_TOKEN is provided
+if [[ -z "$TELEGRAM_API_TOKEN" ]]; then
+  echo -e "\033[1;31mError: No Telegram API Token provided.\033[0m"
+  exit 1
 fi
 
 # Clone the repository
@@ -20,28 +16,29 @@ git clone https://github.com/shantiya401/btc_action_bot.git
 # Change to the project directory
 cd btc_action_bot
 
-# Install Python dependencies
-echo "Installing Python dependencies..."
-pip3 install pyTelegramBotAPI requests pandas ta
-
-# Request for Telegram API Token
-read -p "Please enter your Telegram API Token: " TELEGRAM_API_TOKEN
-
-# Check if TELEGRAM_API_TOKEN is provided
-if [[ -z "$TELEGRAM_API_TOKEN" ]]; then
-  echo "Error: No Telegram API Token provided."
-  exit 1
-fi
-
 # Replace placeholder with the user-provided token in the Python script
 echo "Updating the bot script with the provided Telegram API Token..."
 sed -i "s/YOUR_TELEGRAM_API_TOKEN/$TELEGRAM_API_TOKEN/" bot.py
 
 # Verify token replacement
 if grep -q "YOUR_TELEGRAM_API_TOKEN" bot.py; then
-  echo "Error: Failed to update the bot script with the provided Telegram API Token."
+  echo -e "\033[1;31mError: Failed to update the bot script with the provided Telegram API Token.\033[0m"
   exit 1
 fi
+
+# Display success message
+echo -e "\033[1;33mSuccessfully updated the bot script with the provided Telegram API Token.\033[0m"
+
+# Update package list and install required packages
+echo "Updating package list..."
+sudo apt-get update
+
+echo "Installing required packages..."
+sudo apt-get install -y python3 python3-pip git
+
+# Install Python dependencies
+echo "Installing Python dependencies..."
+pip3 install pyTelegramBotAPI requests pandas ta
 
 # Run the bot using nohup
 echo "Starting the bot using nohup..."
